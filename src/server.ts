05a63ed2447cf17dc67ba601f67b00acf8486354
nitslaszlo <nitslaszlo@gmail.com>
2021-12-15 20:07:12 +0100

    up

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
 import PostController from "./post/post.controller";
@@ -6,6 +6,7 @@ import ReportController from "./report/report.controller";
 import UserController from "./user/user.controller";
 import validateEnv from "./utils/validateEnv";
 
+config(); // Read and set variables from .env file.
 validateEnv();
 
 const app = new App([new PostController(), new AuthenticationController(), new UserController(), new ReportController()]);

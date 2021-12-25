bdf678bd1e76983d1892acd4bf6fad04d5355a52
nitslaszlo <nitslaszlo@gmail.com>
2021-12-25 03:42:28 +0100

    up

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ export default class App {
             res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
             res.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
             // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested, X-Token, Content-Type, Accept Authorization");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, OPTIONS, Accept, Authorization, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers");
             res.setHeader("Access-Control-Allow-Credentials", "true");
             res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
             // if (req.method === "OPTIONS") {

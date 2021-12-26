243896d5a86fe0efbc08d80615cfee6d9cbf0aa7
nitslaszlo <nitslaszlo@gmail.com>
2021-12-26 07:13:57 +0100

    up

--- src/app.ts
+++ src/app.ts
@@ -45,20 +45,6 @@ export default class App {
                 exposedHeaders: ["set-cookie"],
             }),
         );
-        // this.app.use((req, res, next) => {
-        //     // res.header("Access-Control-Allow-Origin", "*");
-        //     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-        //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        //     // Ha "Access-Control-Allow-Credentials", "true", akkor az origin nem lehet "*"!
-        //     res.header("Access-Control-Allow-Credentials", "true");
-        //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        //     // res.header("Set-Cookie", "HttpOnly;Secure;SameSite=None");
-        //     // if (req.method === "OPTIONS") {
-        //     //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        //     //     return res.status(200).json({});
-        //     // }
-        //     next();
-        // });
         this.app.use(loggerMiddleware);
     }
 

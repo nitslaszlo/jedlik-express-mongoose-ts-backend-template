042958d357d384a742b674df94ad1173f2a8bb3f
nitslaszlo <nitslaszlo@gmail.com>
2022-02-20 03:57:47 +0100

    up

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ export default class App {
     }
 
     private connectToTheDatabase() {
-        // Connect MongoDB Atlas:
+        // Connect to MongoDB Atlas:
         // mongoose.connect("mongodb+srv://m001-student:m001-student@sandbox.3fiqf.mongodb.net/VizsgaBackend?retryWrites=true&w=majority");
 
         // Connect to localhost:

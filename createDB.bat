d3bec62c2bd1dbf6d25f22105fed63883cb58644
nitslaszlo <nitslaszlo@gmail.com>
2022-03-14 23:53:25 +0100

    up

--- createDB.bat
+++
@@ -1,4 +0,0 @@
-"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/VizsgaBackend" --collection=recipes --drop --file=data/recipes.json --jsonArray
-"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/VizsgaBackend" --collection=users --drop --file=data/users.json --jsonArray
-
-
---
+++ ingatlanok.bat
@@ -0,0 +1,2 @@
+"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/VizsgaBackend" --collection=kategoriak --drop --file=data/kategoriak.json --jsonArray
+"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://localhost:27017/VizsgaBackend" --collection=ingatlanok --drop --file=data/ingatlanok.json --jsonArray

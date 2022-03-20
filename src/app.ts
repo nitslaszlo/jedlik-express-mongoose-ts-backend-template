6b30c310d37ccbae37276cf9a9db249f7bbb46dc
nitslaszlo <nitslaszlo@gmail.com>
2022-03-20 08:26:05 +0100

    up

--- src/app.ts
+++ src/app.ts
@@ -25,10 +25,25 @@ export default class App {
 
     private connectToTheDatabase() {
         // Connect to MongoDB Atlas, create VizsgaBackend database if not exist::
-        // mongoose.connect("mongodb+srv://m001-student:m001-student@sandbox.3fiqf.mongodb.net/VizsgaBackend?retryWrites=true&w=majority");
+        // mongoose.connect("mongodb+srv://m001-student:m001-student@sandbox.3fiqf.mongodb.net/VizsgaBackend?retryWrites=true&w=majority", err => {
+        //     if (err) {
+        //         console.log("Unable to connect to the server. Please start MongoDB.");
+        //     }
+        // });
 
         // Connect to localhost:27017, create VizsgaBackend database if not exist:
-        mongoose.connect(`mongodb://localhost:27017/VizsgaBackend`);
+        mongoose.connect("mongodb://localhost:27017/VizsgaBackend", err => {
+            if (err) {
+                console.log("Unable to connect to the server. Please start MongoDB.");
+            }
+        });
+
+        mongoose.connection.on("error", error => {
+            console.log(`Mongoose error message: ${error.message}`);
+        });
+        mongoose.connection.on("connected", () => {
+            console.log("Connected to MongoDB server.");
+        });
 
         authorModel.init(); // for populate
     }

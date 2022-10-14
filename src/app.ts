5b230e6464ffe589bb6a2177d141948f803fa273
nitslaszlo <nitslaszlo@gmail.com>
2022-10-14 20:20:24 +0200

    up

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,7 @@ export default class App {
         this.app.use(
             cors({
                 origin: ["https://jedlik-vite-quasar-template.netlify.app", "https://jedlik-vite-ts-template.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"],
+                allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Content-Language", "Expires", "Last-Modified", "Pragma"],
                 credentials: true,
                 exposedHeaders: ["set-cookie"],
             }),
